788f3e92088df410ab26d839a49a8cc4c4708fc0
Peter Durdin <peter.durdin.superworld@gmail.com>
2026-05-11 19:51:27 +0200

    Add basic navigation and gitignore

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import gi
-gi.require_version('Gtk','4.0')
-from gi.repository import Gtk, GLib, GdkPixbuf, Gdk
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk, GLib, Gdk
 import requests
-from PIL import Image
-import io
+
+latest = requests.get("https://xkcd.com/info.0.json").json()
+comic = latest["num"]
+max = latest["num"]
 
 
 class MyWindow(Gtk.ApplicationWindow):
     def __init__(self, **kargs):
         super().__init__(**kargs, title="xkcd Viewer")
-        self.raw = requests.get("https://imgs.xkcd.com/comics/crystal_gazing.png",stream=True)
-        self.image_file = io.BytesIO(self.raw.raw.read())
-        self.im = Image.open(self.image_file)
-        self.w, self.h = self.im.size
-        self.pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(GLib.Bytes.new(self.im.tobytes()), GdkPixbuf.Colorspace.RGB, False, 8, self.w, self.h, self.w * 3)
-        self.pnt = Gdk.Texture.new_for_pixbuf(self.pixbuf)
-        self.img = Gtk.Picture.new_for_paintable(self.pnt)
-        self.set_child(self.img)
+        self.grid = Gtk.Grid()
+        self.set_child(self.grid)
+        self.img = Gtk.Picture.new_for_paintable(fetch_xkcd(comic))
+        self.left = Gtk.Button(label="<")
+        self.right = Gtk.Button(label=">")
+        self.left.connect("clicked", self.left_cl)
+        self.right.connect("clicked", self.right_cl)
+        self.grid.attach(self.img, 0, 0, 2, 2)
+        self.grid.attach(self.left, 0, 3, 1, 1)
+        self.grid.attach(self.right, 1, 3, 1, 1)
         self.present()
 
+    def left_cl(self, _widget):
+        global comic
+        comic -= 1
+        if comic < 1:
+            comic += 1
+        self.img.set_paintable(fetch_xkcd(comic))
+
+    def right_cl(self, _widget):
+        global comic
+        global max
+        comic += 1
+        if comic > max:
+            comic -= 1
+        self.img.set_paintable(fetch_xkcd(comic))
+
+
+def fetch_xkcd(comicN):
+    ciurl = "https://xkcd.com/"+str(comicN)+"/info.0.json"
+    comicInfo = requests.get(ciurl).json()
+    url = comicInfo["img"]
+    raw = requests.get(url, stream=True)
+    image_file = raw.raw.read()
+    return Gdk.Texture.new_from_bytes(GLib.Bytes.new(image_file))
 
 
 def on_activate(app):
@@ -26,6 +54,6 @@ def on_activate(app):
     win.present()
 
 
-app = Gtk.Application(application_id='com.xkcd.viewer')
-app.connect('activate', on_activate)
+app = Gtk.Application(application_id="com.xkcd.viewer")
+app.connect("activate", on_activate)
 app.run(None)
